5b8b22b403def3e812597ad4c00d9525cd62ee29
Andy Arthur <andy@dolthub.com>
2022-07-01 10:25:58 -0700

    standalone oltp_delete_insert

--- oltp_delete_insert.lua
+++ oltp_delete_insert.lua
@@ -1,10 +1,106 @@
-require("oltp_common_copy")
+-- Copyright (C) 2006-2018 Alexey Kopytov <akopytov@gmail.com>
 
-function prepare_statements()
-	prepare_delete_inserts()
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 2 of the License, or
+-- (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, write to the Free Software
+-- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+-- -----------------------------------------------------------------------------
+-- Common code for OLTP benchmarks.
+-- -----------------------------------------------------------------------------
+
+dolt_prepare = prepare
+
+function prepare()
+    sysbench.opt.threads = 1
+    drv = sysbench.sql.driver()
+    con = drv:connect()
+    create_table(drv, con, 1)
+end
+
+function thread_init()
+  drv = sysbench.sql.driver()
+  con = drv:connect()
+end
+
+function thread_done()
+    stmt:close()
+    con:disconnect()
 end
 
 function event()
-	execute_delete_inserts()
+    local id = sysbench.rand.default(1, 10000)
+    local k_val = sysbench.rand.default(1, 10000)
+    local c_val = get_c_value()
+    local pad_val = get_pad_value()
+
+    con:query(string.format("DELETE FROM sbtest1 WHERE id = %d", id))
+    con:query(string.format("INSERT INTO sbtest1 (id, k, c, pad) VALUES " ..
+                            "(%d, %d, '%s', '%s')", id, k_val, c_val, pad_val))
 end
 
+
+-- 10 groups, 119 characters
+local c_value_template = "###########-###########-###########-" ..
+   "###########-###########-###########-" ..
+   "###########-###########-###########-" ..
+   "###########"
+
+-- 5 groups, 59 characters
+local pad_value_template = "###########-###########-###########-" ..
+   "###########-###########"
+
+function get_c_value()
+   return sysbench.rand.string(c_value_template)
+end
+
+function get_pad_value()
+   return sysbench.rand.string(pad_value_template)
+end
+
+function create_table(drv, con, table_num)
+   local id_index_def, id_def
+   local engine_def = ""
+   local extra_table_options = ""
+   local query
+
+   print(string.format("Creating table 'sbtest%d'...", table_num))
+
+   query = [[
+CREATE TABLE sbtest1 (
+  id int NOT NULL AUTO_INCREMENT,
+  k INTEGER DEFAULT '0' NOT NULL,
+  c CHAR(120) DEFAULT '' NOT NULL,
+  pad CHAR(60) DEFAULT '' NOT NULL,
+  PRIMARY KEY (id),
+  KEY k_1 (k)
+) ]]
+
+   con:query(query)
+
+   query = "INSERT INTO sbtest" .. table_num .. "(id, k, c, pad) VALUES"
+   con:bulk_insert_init(query)
+
+   local c_val
+   local pad_val
+
+   for i = 1, 10000 do
+      c_val = get_c_value()
+      pad_val = get_pad_value()
+      query = string.format("(%d, %d, '%s', '%s')",
+                                     i, sb_rand(1, 10000), c_val,
+                                     pad_val)
+      con:bulk_insert_next(query)
+   end
+
+   con:bulk_insert_done()
+end
